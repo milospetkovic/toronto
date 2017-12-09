a6421339ded84ea7009421cda17a63d39c214fc7
petkovicmilos <mail2milos@yahoo.com>
2017-12-09 22:50:47 +0100

    wip - routes..

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Elitasoft - Laravel
                 </div>
 
                 <div class="links">
